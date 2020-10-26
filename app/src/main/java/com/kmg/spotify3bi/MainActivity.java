b4809ad1998e3e01f52b6477b29107e8c6381a6d
Klinsman Maia <klinsman.goncalves@gmail.com>
2020-10-26 09:48:53 -0400

    adicionando musica

--- app/src/main/java/com/kmg/spotify3bi/MainActivity.java
+++ app/src/main/java/com/kmg/spotify3bi/MainActivity.java
@@ -12,9 +12,6 @@ import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    MediaPlayer mediaPlayer;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         Glide.with(this).load("https://img.youtube.com/vi/tc-bxi26GSA/0.jpg").into(imageView);
 
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.watermelon_sugar);
     }
 
-
-
-    public void playMusic(View view) {
-        if(mediaPlayer == null)
-            return;
-        if(mediaPlayer.isPlaying()){
-            mediaPlayer.pause();
-            ((ImageView)view).setImageResource(R.drawable.ic_play_circle_filled_black_24dp);
-        } else {
-            mediaPlayer.start();
-            ((ImageView)view).setImageResource(R.drawable.ic_pause_circle_filled_black_24dp);
-        }
-
-    }
 }
 
 
